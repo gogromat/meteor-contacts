3709657d4e0f68784d13fa8500a44030251f5f0c
gogromat <gogromat@gmail.com>
2013-02-01 13:17:12 -0500

    Added delete functionality for addresses

--- contacts.js
+++ contacts.js
@@ -8,6 +8,7 @@ if (Meteor.isClient) {
 
 
   Template.contacts_view.contacts = function () {
+
     return Contacts.find({}, {sort: {name: 1}});//, address: 1
   };
 
@@ -15,9 +16,6 @@ if (Meteor.isClient) {
   //  var player = Players.findOne(Session.get("selected_player"));
   //  return player && player.name;
   //};
-
-
-
   //Template.player.selected = function () {
   //  return Session.equals("selected_player", this._id) ? "selected" : '';
   //};
@@ -45,7 +43,18 @@ if (Meteor.isClient) {
       }
   });
 
-  Template.contact_row.events({
+
+  // This is a transformation funciton that takes our
+  // array of addresses for a person and returning a new object that 
+  // containt an id of the contact in it as well for further manipulation
+  Template.contact_item.address_objs = function () {
+    var contact_id = this._id;
+    return _.map(this.addresses || [], function (address) {
+      return {contact_id: contact_id, street: address.street};
+    });
+  };
+
+  Template.contact_item.events({
       'click .remove-contact': function() {
         Contacts.remove(this._id);
       },
@@ -53,6 +62,13 @@ if (Meteor.isClient) {
         Contacts.update(this._id, 
           {$push: {addresses: { street: document.getElementById(this._id+'_add_address').value } } });
         document.getElementById(this._id+'_add_address').value = "";
+      },
+      'click .remove-address': function(evt) {
+        var street = this.street;
+        var id     = this.contact_id;
+        Contacts.update(
+          {_id:id}, 
+          {$pull: {addresses: {"street":street} } });
       }
   });
 
